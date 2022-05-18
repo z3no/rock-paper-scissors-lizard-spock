30579278acc8bdf788b9f09e836258b79b2b937b
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-18 17:02:35 +0200

    created 7 variables, to work with later on

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const lizard = document.getElementById('lizard');
+const spock = document.getElementById('spock');
+const reset = document.getElementById('reset');
+const random = document.getElementById('random');
