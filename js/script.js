61b600f0534b940416724309966400c6e49fc403
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-19 10:16:24 +0200

    created a random function that chooses a random value for the computer once you push the button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-const lizard = document.getElementById('lizard');
-const spock = document.getElementById('spock');
-const reset = document.getElementById('reset');
-const random = document.getElementById('random');
+let playerScore = 0;
+let computerScore = 0;
+
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const lizardButton = document.getElementById('lizard');
+const spockButton = document.getElementById('spock');
+const resetButton = document.getElementById('reset');
+
+
+const playersChoices = [rockButton, paperButton, scissorsButton, lizardButton, spockButton];
+const computersChoices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+// Random function that chooses a rock, paper, scissors, lizard, spock value once you push the computers choice button
+const computerButton = document.getElementById('random');
+computerButton.addEventListener('click', random);
+function random() {
+    const choice = document.getElementById('randomChoice');
+    choice.innerText = computersChoices[Math.floor(Math.random() * computersChoices.length)];
+}
+
+
+
